9daca695f3940fae0dcdb7022cdad8971347fbcc
Joao Oliveira <jpsoliveira.me@gmail.com>
2024-01-18 20:48:54 -0300

    chore: add tailwind and config file

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      screens: {
+        sm: "461px",
+      },
+      colors: {
+        "fcc-gray": "#eee",
+      },
+    },
+  },
+  plugins: [],
+};
